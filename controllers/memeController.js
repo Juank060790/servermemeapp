eac9bd1dba92dc6f564fdd1c1267fd60b244324c
JuanCa <juank060790@gmail.com>
2020-08-16 21:02:16 +0700

    final server

--- controllers/memeController.js
+++ controllers/memeController.js
@@ -100,4 +100,45 @@ memeController.getOriginalImages = (req, res, next) => {
     next(err);
   }
 };
+
+memeController.updateMeme = async (req, res, next) => {
+  try {
+    const memeId = req.params.id;
+    // Read data from the json file
+    let rawData = fs.readFileSync("memes.json");
+    let memes = JSON.parse(rawData).memes;
+    const index = memes.findIndex((meme) => meme.id === memeId);
+    if (index === -1) {
+      return utilsHelper.sendResponse(
+        res,
+        400,
+        false,
+        null,
+        new Error("Meme not found"),
+        null
+      );
+    }
+    const meme = memes[index];
+    meme.texts = req.body.texts || [];
+    meme.updatedAt = Date.now();
+
+    // Put text on image
+    await photoHelper.putTextOnImage(
+      meme.originalImagePath,
+      meme.outputMemePath,
+      meme.texts
+    );
+    fs.writeFileSync("memes.json", JSON.stringify({ memes }));
+    return utilsHelper.sendResponse(
+      res,
+      200,
+      true,
+      meme,
+      null,
+      "Meme has been updated!"
+    );
+  } catch (err) {
+    next(err);
+  }
+};
 module.exports = memeController;
--- routes/memeApi.js
+++ routes/memeApi.js
@@ -33,45 +33,4 @@ router.post(
 
 router.put("/:id", memeController.updateMeme);
 
-memeController.updateMeme = async (req, res, next) => {
-  try {
-    const memeId = req.params.id;
-    // Read data from the json file
-    let rawData = fs.readFileSync("memes.json");
-    let memes = JSON.parse(rawData).memes;
-    const index = memes.findIndex((meme) => meme.id === memeId);
-    if (index === -1) {
-      return utilsHelper.sendResponse(
-        res,
-        400,
-        false,
-        null,
-        new Error("Meme not found"),
-        null
-      );
-    }
-    const meme = memes[index];
-    meme.texts = req.body.texts || [];
-    meme.updatedAt = Date.now();
-
-    // Put text on image
-    await photoHelper.putTextOnImage(
-      meme.originalImagePath,
-      meme.outputMemePath,
-      meme.texts
-    );
-    fs.writeFileSync("memes.json", JSON.stringify({ memes }));
-    return utilsHelper.sendResponse(
-      res,
-      200,
-      true,
-      meme,
-      null,
-      "Meme has been updated!"
-    );
-  } catch (err) {
-    next(err);
-  }
-};
-
 module.exports = router;
